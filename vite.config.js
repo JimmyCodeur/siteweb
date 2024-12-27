0b170ddad23ca4613df6c77d6995a4994305edd9
JimmyCodeur <mr.fernandezjimmy@gmail.com>
2024-12-27 10:46:57 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-})
+  base: '/siteweb/' // Remplacez 'siteweb' par le nom de votre dépôt GitHub
+});
