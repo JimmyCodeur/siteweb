297b0847af0783a166bae074ef086886b5c7538f
JimmyCodeur <mr.fernandezjimmy@gmail.com>
2024-12-27 11:39:16 +0100

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/siteweb/' // Remplacez 'siteweb' par le nom de votre dépôt GitHub
+  // base: '/siteweb/' // Remplacez 'siteweb' par le nom de votre dépôt GitHub
 });
