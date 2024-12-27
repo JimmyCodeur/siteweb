2a367a0147b2658e21c9e4b4faa66f4e016ae07d
JimmyCodeur <mr.fernandezjimmy@gmail.com>
2024-12-27 14:20:45 +0100

    Configure build and deploy with secrets

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/siteweb/' // Remplacez 'siteweb' par le nom de votre dépôt GitHub
+  define: {
+    'process.env': process.env,
+  },
 });
